ca84fb4b33d519c0194724a92fac030e64b96d19
Meet Mehta <meet.mehta@think360.ai>
2025-03-17 16:53:46 +0530

    moving on in 7hr course

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,19 +1,27 @@
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from django.shortcuts import get_object_or_404
 
 from core.models import Product
 from .serializers import ProductSerializer
 
-class ProductCreateApiView(generics.CreateAPIView):
+class ProductListCreateApiView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     
     def perform_create(self, serializer):
         # serializer.save(user=self.request.user)
-        print(serializer)
-        serializer.save()
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        
+        if content is None:
+            content = title
+        serializer.save(content=content)
     
     
-product_create_view = ProductCreateApiView.as_view()
+product_list_create_view = ProductListCreateApiView.as_view()
 
 class ProductDetailApiView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
@@ -21,3 +29,54 @@ class ProductDetailApiView(generics.RetrieveAPIView):
     # lookup_fields = 'pk'
     
 product_detail_view = ProductDetailApiView.as_view()
+
+class ProductUpdateApiView(generics.UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_fields = 'pk'
+    
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        if not instance.content:
+            instance.content = instance.title
+        
+    
+product_update_view = ProductUpdateApiView.as_view()
+
+class ProductDeleteApiView(generics.DestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_fields = 'pk'
+    
+    def perform_destroy(self, instance):
+        super().perform_Destroy(instance)
+    
+product_delete_view = ProductDeleteApiView.as_view()
+
+@api_view(['GET','POST'])
+def product_alt_view(request, pk=None, *args, **kwargs):
+    method = request.method
+    
+    if method == 'GET':
+        if pk is not None:
+            obj = get_object_or_404(Product,pk=pk)
+            data = ProductSerializer(obj,many=False).data
+            return Response(data)
+
+        qs = Product.objects.all()
+        data = ProductSerializer(qs, many=True)
+        return Response(data)
+    
+    if method == 'POST':
+        serializer = ProductSerializer(data = request.data)
+        if serializer.is_valid(riase_exception=True):
+            title = serializer.validated_data.get('title')
+            content = serializer.validated_data.get('content') or None
+            
+            if content is None:
+                content = title
+            
+            serializer.save(content=content)
+            return Response(serializer.data)
+        return Response({"invalid":"not good data"},status=400)
+        
